fc2cc2b6571e507b0841040a973fc1955f30e90e
rosemaryto <rosemary_to@ymail.com>
2023-06-09 15:42:18 -0700

    final

---
+++ module4/src/EmployeeFilter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class EmployeeFilter extends React.Component {
+    render() {
+        return (<div>This isn't a placeholder for the filter</div>)
+    }
+}
